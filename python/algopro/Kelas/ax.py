032c193979bcf1e955438599427fa2f72dee29ba
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2021-11-24 07:13:57 +0700

    create calculator in ax.py

--- python/algopro/Kelas/ax.py
+++ python/algopro/Kelas/ax.py
@@ -1,6 +1,25 @@
-flot = 0.74629573
-flota = 0.98629573
+# Create calculator
+class Calculator:
+    def __init__(self, value):
+        self.value = value
+    def power(self, power):
+        return self.value ** power
+    def product(self, product):
+        return self.value * product
+    def sum(self, sum):
+        return self.value + sum
+    def difference(self, difference):
+        return self.value - difference
+    def quotient(self, quotient):
+        return self.value / quotient
+    def remainder(self, remainder): 
+        return self.value % remainder
+    def sqrt(self, sqrt):
+        return self.value ** (1/sqrt)
+    def log(self, log):
+        return self.value ** (1/log)
 
-print("Volume = %0.2f %0.5f %0.3f" %(flot, flota, flot))
-
-
+a=55
+b=3
+calc = Calculator(a)
+print(calc.sum(b))
