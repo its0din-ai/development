fc26e20ff48e78d653d34b77299641a3b63d8396
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2021-12-02 11:21:20 +0700

    :hammer:

--- web/js/script.js
+++ web/js/script.js
@@ -1,1 +1,29 @@
 // Testing Gitmoji
+let $name = "encrypt0r";
+var _email = "encrypt0r@google.com";
+const Fav = "Golang";
+const fav = "R Script"
+let db = [
+        {nama: "elon", namaBelakang: "musk", email: "elon@space.x"},
+        {nama: "mark", namaBelakang: "zuckerberg", email: "mark@meta.com"},
+        {nama: "bill", namaBelakang: "gates", email: "gates@microsoft.com"},
+        {nama: "james", namaBelakang: "bond", email: "bond007@cia.org"}
+]
+let z = 666
+
+{
+    let x = 55
+    let y = 20
+    let z = x + y
+    var xyz = "LOL"
+    console.log(z)
+}
+
+console.log(z)
+console.log($name);
+console.error(_email);
+console.warn(Fav);
+console.info(fav);
+console.log(xyz)
+
+console.table(db);
