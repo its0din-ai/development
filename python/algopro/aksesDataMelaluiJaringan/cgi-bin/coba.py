5ae4444d6da9ad9f3fd5db1cc67a59ba3d7fb9cc
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2021-12-20 11:21:43 +0700

    :boom: DONE

--- python/algopro/aksesDataMelaluiJaringan/cgi-bin/coba.py
+++ python/algopro/aksesDataMelaluiJaringan/cgi-bin/coba.py
@@ -11,22 +11,23 @@ def hitung_umur(lahir):
         bulan += 12
     return tahun, bulan
  
-lahir = datetime.date(1999, 2, 15) # lahir 20 September 2000
+lahir = datetime.date(2002, 5, 15) # lahir 20 September 2000
 thn, bln = hitung_umur(lahir)
 nama = "Wahyu"  # ganti dengan nama panggilan Anda
- 
-htm = f"""<!DOCTYPE html>
- 
+nim = "L200210056" # ganti dengan NIM Anda
+
+htm = f"""
+<!DOCTYPE html>
 <html>
 <head>
 <title>Server dengan konten dinamis</title>
 </head>
 <body>
-<p>{nama} lahir pada tanggal {lahir}.</p>
+<p>Saya adalah {nama} dengan NIM {nim}. Saya lahir pada tanggal {lahir}.</p>
 <p>Hari ini adalah tanggal {datetime.date.today()}.</p>
-<p>Karena itu Doni berumur {thn} tahun {bln} bulan.</p>
+<p>Karena itu saya berumur {thn} tahun {bln} bulan.</p>
 <p>Terima kasih</p>
 </body>
 </html>"""
- 
+
 print(htm)
