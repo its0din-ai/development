c36d6b69f0f9bb0bb86269d2153b249b5174c45c
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2021-12-20 09:39:03 +0700

    :twisted_rightwards_arrows: menambah kondisi

--- python/praktikumAlgopro/modul10/kegiatan3/client.py
+++ python/praktikumAlgopro/modul10/kegiatan3/client.py
@@ -1,4 +1,4 @@
-import socket
+import socket, os, sys
 
 host = 'localhost'
 port = 666
@@ -13,9 +13,26 @@ soc.send(alas.encode())
 print(f"[Sudah Dicatat] Alas Jajar Genjang = {alas}\n")
 tinggi = input("Berapa Tingginya => ")
 soc.send(tinggi.encode())
-print(f"[Sudah Dicatat] Tinggi Jajar Genjang = {tinggi}")
-print("------------------------------------------------")
-hasil = soc.recv(1024).decode()
-print(f"""Hasil kalkulasi Luas Jajar Genjang
-dengan tinggi {tinggi} dan alas {alas} adalah => {hasil}
-""")
+def pil():
+    print(f"[Sudah Dicatat] Tinggi Jajar Genjang = {tinggi}")
+    print("------------------------------------------------")
+    print("Selanjutnya mau ngapain?")
+    print("""1. Hitung
+2. Keluar""")
+x = 0
+pil()
+pilihan = input("Pilihanmu => ")
+if pilihan == "1":
+    hasil = soc.recv(1024).decode()
+    print(f"""Hasil kalkulasi Luas Jajar Genjang
+dengan tinggi {tinggi} dan alas {alas} adalah => {hasil}""")
+    print("Terimakasih..")
+    soc.close()
+elif pilihan == "2":
+    print("Kembali Lagi yaa..")
+    soc.close()
+    quit()
+else:
+    print("Menu Tidak Tersedia")
+    print("Silahkan Coba Lagi")
+    soc.close()
--- python/praktikumAlgopro/modul10/kegiatan3/server.py
+++ python/praktikumAlgopro/modul10/kegiatan3/server.py
@@ -1,10 +1,5 @@
 import socket
 
-# Luas Jajar Genjang, NIM Akhir = 6
-def hitungLuas(alas, tinggi):
-    luas = alas * tinggi
-    return luas
-
 host = 'localhost'
 port = 666
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,16 +7,21 @@ soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 soc.bind((host, port))
 soc.listen()
 
-print("[Waiting for Connection]")
-conn, addr = soc.accept()
-print(f"[Incoming Connection from {addr}] Server Dimulai!")
+def hitung(alas, tinggi):
+    hasil = alas * tinggi
+    strHasil = str("{:g}".format(hasil))
+    print(f"hasil => {strHasil}")
+    conn.send(strHasil.encode())
 
-alas = float(conn.recv(1024).decode())
-tinggi = float(conn.recv(2048).decode())
-print(f"alas => {alas}")
-print(f"tinggi => {tinggi}")
+def main():
+    alas = float(conn.recv(1024).decode())
+    tinggi = float(conn.recv(2048).decode())
+    print(f"alas => {alas}")
+    print(f"tinggi => {tinggi}")
+    return hitung(alas, tinggi)
 
-hasil = hitungLuas(alas, tinggi)
-strHasil = str("{:g}".format(hasil))
-print(f"hasil => {strHasil}")
-conn.send(strHasil.encode())
+while True:
+    print("[Waiting for Connection]")
+    conn, addr = soc.accept()
+    print(f"[Incoming Connection from {addr}] Server Dimulai!")
+    main()
