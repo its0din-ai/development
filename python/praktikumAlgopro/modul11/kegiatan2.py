74db65f4c2d5e2e028f703e2257a11c5589a8b50
its0din-ai <wahyu403.py@gmail.com>
2021-12-28 04:29:02 -0500

    :white_check_mark: Praktikum 11 Done

--- python/praktikumAlgopro/modul11/kegiatan2.py
+++ python/praktikumAlgopro/modul11/kegiatan2.py
@@ -1,23 +1,57 @@
 from tkinter import Tk, Label, Entry, Button, StringVar
-from tkinter.messagebox import showinfo
 
 gui2 = Tk(className="Kalkulator Sederhana")
+gui2.geometry("854x550")
+
 labAngka1 = Label(gui2, text="ANGKA 1")
-labAngka1.grid(padx=30, pady=30, sticky = "W", row=0, column=0)
+labAngka1.grid(padx=(250, 10), pady=80, sticky = "W", row=0, column=0)
 
 angka1 = StringVar()
-entryAngka1 = Entry(gui2, textvariable=angka1)
-entryAngka1.grid(padx=(10, 10), sticky = "W", row=0, column=1, ipady=15)
+entryAngka1 = Entry(gui2, textvariable=angka1, justify="right")
+entryAngka1.grid(sticky = "W", row=0, column=1, ipady=20, ipadx=40)
 
 
 labAngka2 = Label(gui2, text="ANGKA 2")
-labAngka2.grid(padx=30, pady=10, sticky = "W", row=1, column=0)
+labAngka2.grid(padx=(250, 10), sticky = "W", row=1, column=0)
 
 angka2 = StringVar()
-entryAngka2 = Entry(gui2, textvariable=angka2)
-entryAngka2.grid(padx=(10, 10), sticky = "W", row=1, column=1, ipady=15)
+entryAngka2 = Entry(gui2, textvariable=angka2, justify="right")
+entryAngka2.grid(sticky = "W", row=1, column=1, ipady=20, ipadx=40)
 
-# BELUM SELESAI !!
-# Lanjutin nanti
+def tambah():
+    kalkulasi = int(angka1.get()) + int(angka2.get())
+    hasil = Label(gui2, text=("HASIL")).grid(padx=(0, 10), sticky = "E", row=4, column=0)
+    hasil = Label(gui2, text=str(kalkulasi)).grid(padx=(0, 10), sticky = "E", row=4, column=1)
+    return hasil
+
+def kurang():
+    kalkulasi = int(angka1.get()) - int(angka2.get())
+    hasil = Label(gui2, text=("HASIL")).grid(padx=(0, 10), sticky = "E", row=4, column=0)
+    hasil = Label(gui2, text=str(kalkulasi)).grid(padx=(0, 10), sticky = "E", row=4, column=1)
+    return hasil
+
+def kali():
+    kalkulasi = int(angka1.get()) * int(angka2.get())
+    hasil = Label(gui2, text=("HASIL")).grid(padx=(0, 10), sticky = "E", row=4, column=0)
+    hasil = Label(gui2, text=str(kalkulasi)).grid(padx=(0, 10), sticky = "E", row=4, column=1)
+    return hasil
+
+def bagi():
+    kalkulasi = int(angka1.get()) / int(angka2.get())
+    hasil = Label(gui2, text=("HASIL")).grid(padx=(0, 10), sticky = "E", row=4, column=0)
+    hasil = Label(gui2, text=str(kalkulasi)).grid(padx=(0, 10), sticky = "E", row=4, column=1)
+    return hasil
+
+plus = Button(gui2, text="+", command=tambah)
+plus.grid(row=3, column=0, sticky="W", padx=(250, 0), pady=80, ipadx=15, ipady=15)
+
+mins = Button(gui2, text="-", command=kurang)
+mins.grid(row=3, column=1, sticky="W", padx=(40, 0), pady=80, ipadx=15, ipady=15)
+
+mult = Button(gui2, text="X", command=kali)
+mult.grid(row=3, column=1, sticky="W", padx=(150, 0), pady=80, ipadx=15, ipady=15)
+
+divs = Button(gui2, text=":", command=bagi)
+divs.grid(row=3, column=2, sticky="W", padx=(0, 0), pady=80, ipadx=15, ipady=15)
 
 gui2.mainloop()
---
+++ python/praktikumAlgopro/modul11/kegiatan3.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, Label, Entry, Button, StringVar
+
+gui3 = Tk(className="Luas Geometri")
+gui3.geometry("850x400")
+
+judul = Label(gui3, text="Luas Jajar Genjang", font=("Poppins", 20, "bold")).place(x=20, y=20)
+desk = Label(gui3, text="""Jajar genjang atau jajaran genjang adalah bangun datar dua dimensi yang dibentuk oleh
+dua pasang rusuk yang masing-masing sama panjang dan sejajar dengan pasangannya,
+dan memiliki dua pasang sudut yang masing-masing sama besar dengan sudut di hadapannya.""", font=("Poppins", 10, "bold"), justify="left").place(x=20, y=60)
+
+teksPar1 = Label(gui3, text="Panjang sisi 1").place(x=20, y=120)
+alas = StringVar()
+entPar1 = Entry(gui3, textvariable=alas).place(x=130, y=120)
+
+teksPar2 = Label(gui3, text="Panjang sisi 1").place(x=20, y=150)
+tinggi = StringVar()
+entPar2 = Entry(gui3, textvariable=tinggi).place(x=130, y=150)
+
+def luas():
+    luas = int(alas.get()) * int(tinggi.get())
+    hasil = Label(gui3, text=("LUAS =")).place(x=120, y=205)
+    hasil = Label(gui3, text=str(luas)).place(x=175, y=205)
+    return hasil
+
+butLuas = Button(gui3, text="Hitung", command=luas).place(x=20, y=200)
+
+gui3.mainloop()
