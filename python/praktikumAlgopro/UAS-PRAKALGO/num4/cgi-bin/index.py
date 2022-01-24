0a5c1fe072107e3423212e67d32257240c1434b4
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2022-01-24 11:01:58 +0700

    :fire: masih UAS NGAB?

---
+++ python/praktikumAlgopro/UAS-PRAKALGO/num4/cgi-bin/index.py
@@ -0,0 +1,17 @@
+htm = f"""
+<!DOCTYPE html>
+ 
+<html>
+<head>
+<title>UAS</title>
+</head>
+<body>
+<h1>LAYANAN WEB DENGAN CGI</h1>
+<p>Saya mengakses informasi ini melalui program python.Saya mengerjakan Saya mengerjakan UAS ini secara mandiri, hasil kerja saya sendiri, terhindar dari copy paste hasil kerja orang lain</p>
+<p>Nama: JON DOE</p>
+<p>NIM: L666</p>
+<p>Terima kasih</p>
+</body>
+</html>"""
+ 
+print(htm)
